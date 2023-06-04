b583631c062fe77759162a0c257f695ab744357f
Hany Sameh <han798348@gmail.com>
2023-06-04 03:48:44 +0300

    add colors and assets

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xffF9443A);
+const Color secondPrimaryColor = Color(0xffFA5F31);
+const Color darkBlack = Color(0xff0A0A0A);
+const Color lightBlack = Color(0xff121212);
+const Color darkGrey = Color(0xff3D3D3D);
+const Color blue = Color(0xff1458E1);
+const Color royalFuchsia = Color(0xffC72396);
+const Color promiscuousPink = Color(0xffB111F1);
+const Color darkYellow = Color(0xffCB915D);
+const Color chocolateMelange = Color(0xff311302);
